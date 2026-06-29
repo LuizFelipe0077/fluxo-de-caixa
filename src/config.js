979c4c039ea39d6409d34a7c97570b8708da7868
Luiz Felipe Vieira <luiz_felipe.net@hotmail.com>
2026-06-29 17:21:52 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,5 +6,5 @@
  * (termina em /exec). Não há mais modo de demonstração.
  */
 export const CONFIG = Object.freeze({
-  URL_WEB_APP: 'https://script.google.com/macros/s/AKfycbx-OQVbU53N1zh8BwShnMC2BJ5jPkGdRobwdzjFjnReusMrMiqbl24GoYIJSorgNA-Q/exec'
+  URL_WEB_APP: 'https://script.google.com/macros/s/AKfycbz2OWtARggi5av3pec5yHXyofnLc9LlvRqpczzA-gg06fYL0-0Y41_MdE7_rmJc1wwT/exec'
 });
